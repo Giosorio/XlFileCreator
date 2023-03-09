5243a2789579dd0ee66247b8b1409251c5617bf4
Giovanni <giosorio29@gmail.com>
2023-03-09 00:29:04 +0000

    DATA VALIDATION VERSION 2, CLASS DataValidationConfig1 and CLASS DataValidationConfig2 WORKING

--- xlfilecreator/data_validation_config2_func.py
+++ xlfilecreator/data_validation_config2_func.py
@@ -1,23 +1,8 @@
 import pandas as pd
-from xlsxwriter.utility import xl_col_to_name
 
 from typing import Optional, Dict, List
 
 from .data_validation_typing import DataValDict, Header
-
-
-# def config2(df_config2: pd.DataFrame, df_settings: pd.Dataframe)->pd.Dataframe:
-
-#     header_values = df_settings.loc['HEADER'].tolist()
-#     apply_to_xlcolumn = []
-
-#     config_rows = df_config2.shape[0]
-#     for row in range(config_rows):
-#         hd_to_apply = df_config2.loc[row, 'apply_to']
-#         idx_number_col = header_values.index(hd_to_apply)
-#         apply_to_xlcolumn.append(xl_col_to_name(idx_number_col))
-
-#     df_config2['apply_to_xlcolumn'] = apply_to_xlcolumn
 
 
 def create_opts_dict(opts_settings:pd.Series) -> Dict[str,str]:
