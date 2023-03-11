e5c9ddf2690bd432806bfa1f8963919906ccae3d
Giovanni <giosorio29@gmail.com>
2023-03-11 19:48:22 +0000

    REMOVE data_validation_config2_func.py

--- xlfilecreator/data_validation.py
+++ xlfilecreator/data_validation.py
@@ -5,7 +5,7 @@ from abc import ABC, abstractclassmethod
 from typing import List, Dict, Tuple, Optional, Union
 
 from .data_validation_config1_func import get_data_validation_dict,clean_df_data_validation
-from .data_validation_config2_func import get_data_validation_dict_config2
+# from .data_validation_config2_func import get_data_validation_dict_config2
 from .data_validation_typing import Header, SourceDict, SingleOptionsDict, DataValDict
 
 
@@ -73,7 +73,39 @@ class DataValidationConfig2(DataValidationConfiguration):
         else:
             self.picklists = df_picklists
             self.dropdown_list_sheet = dropdown_list_sheet
-            self.data_validation_dict = get_data_validation_dict_config2(df_dvconfig2)
+            self.data_validation_dict = DataValidationConfig2.get_data_validation_dict_config2(df_dvconfig2)
             self.data_val_headers = self.data_validation_dict.keys()
 
-    
+    @staticmethod
+    def create_opts_dict(opts_settings:pd.Series) -> Dict[str,str]:
+        try:
+            opts_dict1={'validate': opts_settings['validate'],
+                    'source': opts_settings['source'],
+                    'error_type': opts_settings['error_type'],
+                    'input_title': opts_settings['input_title'],
+                    'input_message': opts_settings['input_message'],
+                    'error_title': opts_settings['error_title'],
+                    'error_message': opts_settings['error_message']}
+        except KeyError as ke:
+            print('Header not recognised')
+            raise ke
+        
+        opts_dict = {opt:value for opt, value in opts_dict1.items() if value != ''}
+
+        return opts_dict 
+
+    @staticmethod
+    def get_data_validation_dict_config2(df_dvconfig2: pd.DataFrame)->DataValDict:
+
+        data_validation_opts_dict = {}
+        config_rows = df_dvconfig2.shape[0]
+        for row in range(config_rows):
+            hd_to_apply = df_dvconfig2.loc[row, 'apply_to']
+            opts_dict = DataValidationConfig2.create_opts_dict(df_dvconfig2.loc[row])
+            data_validation_opts_dict[hd_to_apply] = opts_dict
+
+        return data_validation_opts_dict
+
+
+
+        
