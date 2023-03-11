76c9ec7592f307d8dea3a5b3d7848f879c2a7fe1
Giovanni <giosorio29@gmail.com>
2023-03-11 01:17:54 +0000

    UPDATE conditional_formatting.py ADDING CondFormatting class. xlfiletemp.py HAS NOT BEEN UPDATED YET TO READ THE CONDITIONAL FORMATTING TAB FROM GOOGLE SHEETS

--- xlfilecreator/conditional_formatting.py
+++ xlfilecreator/conditional_formatting.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import xlsxwriter
 
-from typing import List
+from collections import namedtuple
+from typing import List, Dict
 
 from .formats import format_dict
 from .utils_func import EXTRA_ROWS
@@ -36,6 +37,58 @@ df: pd.DataFrame, df_settings: pd.DataFrame, data_index: int, allow_input_extra_
 
 
 
-# def set_conditional_formatting():
-#     highlight_mandatory()
+class CondFormatting:
 
+    def __init__(self, df_condf: pd.DataFrame, df: pd.DataFrame) -> None:
+        self.df_condf = CondFormatting.df_condf_validation(df_condf, df)
+
+    @staticmethod
+    def df_condf_validation(df_condf: pd.DataFrame, df: pd.DataFrame)->pd.DataFrame:
+        
+        if df_condf is None:
+            return None
+
+        df_condf['valid_apply_to'] = [hd in df.loc['HEADER'].tolist() for hd in df_condf['apply_to']]
+        ### Valid type should be validated against a list of accepted values for future versions
+        df_condf['valid_type'] = [t != '' for t in df_condf['type']]
+        df_condf['valid_criteria'] = [c != '' for c in df_condf['criteria']]
+        df_condf['valid_format'] = [format_dict[f] if f in format_dict.keys() else False for f in df_condf['format']]
+        
+        ### Overall Validation
+        df_condf = df_condf[df_condf['valid_format']!=False]
+        df_condf['overall_validation'] = [all((v_apply_to, v_type, v_criteria)) for v_apply_to, v_type, v_criteria in zip(df_condf['valid_apply_to'], df_condf['valid_type'], df_condf['valid_criteria'])]
+        df_condf = df_condf[df_condf['overall_validation']==True]
+
+        return df_condf
+
+    @staticmethod
+    def create_opts_dict(wb, opts_settings:pd.Series) -> Dict[str,str]:
+
+        opts_dict={'type': opts_settings['type'],
+            'criteria': opts_settings['criteria'],
+            'format': eval(opts_settings['valid_format'])}
+
+        return opts_dict
+
+    def set_conditional_formatting(self, wb: xlsxwriter.workbook.Workbook, ws: xlsxwriter.worksheet.Worksheet, df: pd.DataFrame) -> None:
+        
+        """
+        Set up conditional formatting
+        Parameters:
+        ws: worksheet
+        df: dataframe used to create the template header=None
+        self.df_condf: dataframe containing the configuration for the data validation
+        """
+
+        if self.df_condf is None:
+            return None
+
+        initial_index = df.index.tolist().index('')  ## df index 0 = excel row 1
+        last_row_index = df.shape[0] - 1
+        header_list = df.loc['HEADER'].tolist()
+
+        for row in range(self.df_condf.shape[0]):
+            hd_apply_to = self.df_condf.loc[row, 'apply_to']
+            col_idx = header_list.index(hd_apply_to)
+            opts_dict = CondFormatting.create_opts_dict(wb, self.df_condf.loc[row])
+            ws.conditional_format(initial_index, col_idx, last_row_index, col_idx, opts_dict)
