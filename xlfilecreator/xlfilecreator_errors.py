3ee05764a626eb0c12f18dc3507e6104a2712dac
Giovanni <giosorio29@gmail.com>
2023-03-12 13:14:48 +0000

    REMOVE MainSheetERROR and using a KeyError instead

--- xlfilecreator/xlfilecreator_errors.py
+++ xlfilecreator/xlfilecreator_errors.py
@@ -8,10 +8,4 @@ class HeaderIndexNotIdentified(Exception):
         print(errormessage)
 
 
-class MainSheetNoData(Exception):
-    def __init__(self, *args: object) -> None:
-        errormessage = 'MAIN_SHEET is not readable\nPossible Problems:\nCONFIG_MANAGER row does not have any values\nThe HEADER row does not match any values in the IMPORT_FILE sheet\n'
-        print(errormessage)
-
-
 
