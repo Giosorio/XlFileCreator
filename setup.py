16a4c5de8fc394dd0118a46f376745fa7e8d42a3
Giovanni <giosorio29@gmail.com>
2023-03-13 11:08:46 +0000

    UPDATE setup.py > 0.2 version

--- setup.py
+++ setup.py
@@ -4,10 +4,9 @@ from setuptools import find_packages, setup
 setup(
     name='xlfilecreator',
     packages=find_packages(include=['xlfilecreator']),
-    version='0.1',
-    description='Class XlFileTemp that splits a google sheet workbook on the basis of the values in one of the columns creating multiple password protected excel files',
+    version='0.2',
+    description='Class XlFileTemp that splits a google sheet workbook on the basis of the values in one of the columns creating multiple password protected excel files, It includes dropdown lists, and conditional formatting',
     author='Giovanni Osorio',
     licence='MIT',
     install_requires=['pandas', 'openpyxl', 'xlsxwriter'],
-    # test_suite='tests',
 )
