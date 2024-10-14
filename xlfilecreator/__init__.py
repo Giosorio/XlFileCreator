813ab8ac9294837ebf061591dfaee6660ed9cc80
Giovanni <giosorio29@gmail.com>
2024-10-14 22:11:34 +0100

    update init.py

--- xlfilecreator/__init__.py
+++ xlfilecreator/__init__.py
@@ -1,4 +1,5 @@
 from .data_validation import *
+from .create_xl_file_multiple_templaes import create_xl_file_multiple_temp
 from .encrypt_xl import PackageMsofficeMissing, _check_msoffice_installed, set_password, create_password
 from .formats import format_dict
 from .xlfiletemp import XlFileTemp
