49e7c02bae5d40b576eb48d94b703ce069b62f8c
Giovanni <giosorio29@gmail.com>
2024-10-28 21:33:43 +0000

    VERSION 0.5 Performance improvement, split_by_range, Generate Excel files with multiple templates

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 setup(
     name='xlfilecreator',
     packages=find_packages(include=['xlfilecreator','tqdm']),
-    version='0.44',
+    version='0.5',
     description='Class XlFileTemp that splits a google sheet workbook on the basis of the values in one of the columns creating multiple password protected excel files, It includes dropdown lists, and conditional formatting',
     author='Giovanni Osorio',
     licence='MIT',
